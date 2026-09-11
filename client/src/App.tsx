baf92f5987cc251e260b1a1f11cc80ad67b0cb78
s8757520-sys <s8757520@gmail.com>
2026-09-12 01:47:23 +0300

    Add Prettier to client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ function App() {
     <div className="min-h-screen bg-slate-900 text-white flex items-center justify-center">
       <h1 className="text-3xl font-bold">Ekso работает</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
